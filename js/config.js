8b1183daa004ad18555488e5af417b34fca39889
pupr445 <pupr445@gmail.com>
2026-07-12 12:13:48 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,6 +4,6 @@
 // Nilai ini AMAN untuk ditaruh di frontend (bukan rahasia) selama RLS aktif.
 
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://vzvcawezlkqhfltfgdux.supabase.co",",
+  SUPABASE_URL: "https://vzvcawezlkqhfltfgdux.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6dmNhd2V6bGtxaGZsdGZnZHV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4MDk0NjQsImV4cCI6MjA5OTM4NTQ2NH0.iG8UFZ-Fco5ZQGUS0p1yRVkV99IPPOO4naKKnOrdreg",
 };
